f07beb487c16be78d3b14828f27dbacd7b382554
Grant Ramsay <seapagan@gmail.com>
2022-11-30 20:54:55 +0000

    start setting up routes and tests
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- project/app/main.py
+++ project/app/main.py
@@ -1,6 +1,6 @@
 import logging
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 from fastapi import FastAPI
 
@@ -10,6 +10,9 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )
 
     return application
 
